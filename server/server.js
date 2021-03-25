c1ad4b64e2ac487a4ef9132b60dc0161a76bf24b
emeksthecreator <58275378+ErnestCodes@users.noreply.github.com>
2021-03-25 07:17:29 -0700

    Create API'S & ROUTES

--- server/server.js
+++ server/server.js
@@ -10,6 +10,18 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
+// Define Routes --> Users Routes
+app.use("/api/users", require("./routes/api/users"));
+
+// Define Routes --> Auth Routes
+app.use("/api/auth", require("./routes/api/auth"));
+
+// Define Routes --> Profile Routes
+app.use("/api/profile", require("./routes/api/profile"));
+
+// Define Routes --> Posts Routes
+app.use("/api/posts", require("./routes/api/posts"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
