fc1c9db00e317d561169bb65b2f1db746a6c8e16
ErnestCodes <emmycontacts@gmail.com>
2021-06-06 21:51:13 +0100

    Update Posts.js

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -24,4 +24,26 @@ const PostSchema = new Schema({
       },
     },
   ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
 });
