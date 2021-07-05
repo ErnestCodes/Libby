304d5761bf7b3d8650a80eb64fa040f9179bd6f2
ErnestCodes <emmycontacts@gmail.com>
2021-07-05 13:39:28 +0100

    Protect dashboard route
    By creating the a PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import "./App.css";
 // Redux
 import { Provider } from "react-redux";
@@ -30,6 +32,11 @@ function App() {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute
+                exact
+                path="/dashboard"
+                comp={Dashboard}
+              ></PrivateRoute>
             </Switch>
           </section>
         </Fragment>
---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const PrivateRoute = ({
+  comp: Components,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      !isAuthenticated && !loading ? (
+        <Redirect to="/login" />
+      ) : (
+        <Components {...props} />
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
