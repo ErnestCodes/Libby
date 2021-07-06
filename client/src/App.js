8db86643c29040c820917713980d2d5e3a6c0e49
ErnestCodes <emmycontacts@gmail.com>
2021-07-06 21:53:41 +0100

    Add network notification

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,19 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
+import Notification from "./components/layout/Notification";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
-
 function App() {
   if (localStorage.token) {
     setAuthToken(localStorage.token);
@@ -28,6 +30,7 @@ function App() {
           <Navbar />
           <Route exact path="/" component={Landing} />
           <section className="container">
+            <Notification />
             <Alert />
             <Switch>
               <Route exact path="/register" component={Register} />
